2df073d42e1cc0da1a3ecc29825467eda8c29376
bumsu Choe <hotsem100@gmail.com>
2024-05-07 19:57:13 +0900

    dirty checking or merge -> merge is dirty

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {   // merge와 동작 방식이 같다.
+        // merge를 사용할 경우 모든 속성이 변경되는 불상사가 발생할 수도 있다.
+        // 바꾸고 싶은 값이 name 뿐이라면 merge를 했을 때 나머지도 다 null이 되어 버린다.
+        // merge는 쓰지 않는다. 라고 생각하자.
+        Item findItem = itemRepository.findOne(itemId);  // 이것은 영속 상태이다.
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
---
+++ src/main/java/jpabook/jpashop/service/UpdateItemDto.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.service;
+
+public class UpdateItemDto {
+    // 파라미터가 많으면 사용할 것
+}
